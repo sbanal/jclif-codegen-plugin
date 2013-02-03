475b68281c88ed213fe2de1dae7cde3e4fd1b6a4
Stephen Lou Banal <stephen.banal@gmail.com>
2013-02-03 16:49:35 +1100

    Added code to support project classpath dependency loading

--- src/main/java/org/jclif/plugin/maven/JclifCodeGenerator.java
+++ src/main/java/org/jclif/plugin/maven/JclifCodeGenerator.java
@@ -1,3 +1,22 @@
+/** 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.jclif.plugin.maven;
 
 import java.io.IOException;
@@ -16,33 +35,34 @@ import org.jclif.plugin.CodeGenerator;
  * @author Stephen Lou Banal
  * 
  * @goal generate-config
- * @phase process-classes
+ * @requiresProject true
+ * @phase process-classes 
+ * @configurator include-project-dependencies
+ * @requiresDependencyResolution compile+runtime
  */
 public class JclifCodeGenerator extends AbstractMojo {
 
 	/**
 	 * Package name of the command handler definitions.
 	 * 
-	 * @parameter	expression="${commandHandlerPackage}" 
+	 * @parameter	expression="${commandHandlerPackage}" default-value=""
 	 * @required
 	 */
 	private String commandHandlerPackage = "";
 	
 	/**
-	 * @parameter	expression="${applicationName}"
+	 * @parameter	expression="${applicationName}" 
 	 * @required 
 	 */
 	private String applicationName;
 	
 	/**
-	 * @parameter	expression="${sourceMainDirectory}"
-	 * @required 
+	 * @parameter	expression="${sourceMainDirectory}" default-value="src/main/java"
 	 */
 	private String sourceMainDirectory;
 	
 	/**
-	 * @parameter	expression="${targetDirectory}"
-	 * @required 
+	 * @parameter	expression="${targetDirectory}"  default-value="target/classes"
 	 */
 	private String targetDirectory;
 	
